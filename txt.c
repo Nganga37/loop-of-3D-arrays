a14917491c1e76fb01dbc971efb8055b9dc81570
Nganga37 <karokinathan46@gmail.com>
2024-10-25 20:06:27 +0300

    Update txt.c

--- txt.c
+++ txt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    // Change the array name to 'matrix'
+    
     int matrix[2][3][4] = {
         { {85, 85, 65, 32}, {14, 75, 56, 29}, {64, 35, 78, 12} },
         { {45, 23, 67, 89}, {58, 94, 32, 11}, {72, 84, 91, 33} }
